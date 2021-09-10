ffd8364a4dadec85eaf6f1ad78cf537b5503588b
Nilo Marcos <nilo.n2m15@gmail.com>
2021-09-10 13:56:30 -0300

    styling informations on screen

--- src/screens/Details/index.js
+++ src/screens/Details/index.js
@@ -1,12 +1,13 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import {View, Text, Alert} from 'react-native';
-
+import { Ionicons } from "@expo/vector-icons";;
 import api from '../../services/api';
+
+import {ContainerAll, ContainerBack, ContainerInfo, Photo, Name, Gender, Status, Species} from './styles';
 
 export default function Details({ navigation, route}){
   const { id } = route.params;
   
-  const [ characters, setCharacters ] = useState(undefined);
+  const [ characters, setCharacters ] = useState([]);
 
   useEffect(() => {
     async function loadCharacters() {
@@ -20,8 +21,17 @@ export default function Details({ navigation, route}){
   }, []);
 
   return (
-    <View style={{marginTop: 15}}>
-      <Text></Text>
-    </View>
+    <ContainerAll>
+      <ContainerBack>
+        <Ionicons name="arrow-back-outline" size={25} />
+      </ContainerBack>
+      <ContainerInfo>
+        <Photo source={{ uri: characters.image }} />
+        <Name>{characters.name}</Name>
+        <Status>{characters.status}</Status>
+        <Species>{characters.species}</Species>
+        <Gender>{characters.gender}</Gender>
+      </ContainerInfo>
+    </ContainerAll>
   );
 }
---
+++ src/screens/Details/styles.js
@@ -0,0 +1,22 @@
+import styled from 'styled-components/native';
+
+export const ContainerAll = styled.View``;
+
+export const ContainerBack = styled.View``;
+
+export const BackButton = styled.TouchableOpacity``;
+
+export const ContainerInfo = styled.View``;
+
+export const Photo = styled.Image`
+  width: 80px;
+  height: 80px;
+`;
+
+export const Name = styled.Text``;
+
+export const Gender = styled.Text``;
+
+export const Status = styled.Text``;
+
+export const Species = styled.Text``;
